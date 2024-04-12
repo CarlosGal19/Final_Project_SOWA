67e39da198bbee68a5a8677f3ad4c5abb777cff4
Carlos Alejandro Galindo Islas <utm22030587@utma.edu.mx>
2024-04-11 23:06:07 -0600

    feat: add get and post functions

---
+++ queries/employee.queries.js
@@ -0,0 +1,26 @@
+const db = require("../db.js");
+
+const Employee = db.employees;
+
+const getEmployees = async (req, res) => {
+  try {
+    const employees = await Employee.findAll();
+    res.json(employees);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const createEmployee = async (req, res) => {
+  try {
+    const employee = await Employee.create(req.body);
+    res.json(employee);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = {
+    getEmployees,
+    createEmployee,
+};
