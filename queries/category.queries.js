28ab5ac61d3879345d6b0cdbb37bc73113f3b365
Carlos Alejandro Galindo Islas <utm22030587@utma.edu.mx>
2024-04-11 23:05:29 -0600

    feat: add get and post functions

---
+++ queries/category.queries.js
@@ -0,0 +1,26 @@
+const db = require("../db.js");
+
+const Category = db.categories;
+
+const getCategories = async (req, res) => {
+  try {
+    const categories = await Category.findAll();
+    res.json(categories);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const createCategory = async (req, res) => {
+  try {
+    const category = await Category.create(req.body);
+    res.json(category);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = {
+    getCategories,
+    createCategory,
+};
